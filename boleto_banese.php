c1ad10d6b3c05d057a06499c4a86ff5e4314fb92
Igor Paiva Oliveira <igorpaivaolivera@gmail.com>
2016-09-01 07:56:10 -0300

    Add files via upload

--- boleto_banese.php
+++ boleto_banese.php
@@ -1,21 +1,21 @@
 <?php
 // DADOS DO BOLETO PARA O SEU CLIENTE
-$data_venc 			= formataData($boleto["vencimento"], 'br', 0);  // Prazo de X dias OU informe data: "13/04/2006"; 
-$valorAux 			= str_replace(".", ",",$boleto["valor"]);
-$valor_cobrado 		= $valorAux; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+$data_venc 			= "";  											// Prazo de X dias OU informe data: "13/04/2006"; 
+$valorAux 			= str_replace(".", ",",1.00);
+$valor_cobrado 		= $valorAux; 									// Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
 $valor_cobrado 		= str_replace(",", ".",$valor_cobrado);
 $valor_boleto 		= number_format($valor_cobrado, 2, ',', '');
 
-$dadosboleto["nosso_numero"] 		= $boleto["idboleto"];  // Nosso numero sem o DV - REGRA: Máximo de 8 caracteres!
-$dadosboleto["numero_documento"] 	= $boleto["idboleto"];	// Num do pedido ou do documento
-$dadosboleto["data_vencimento"] 	= $data_venc; //formataData($boleto["vencimento"], 'br', 0); // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
-$dadosboleto["data_documento"] 		= date("d/m/Y"); // Data de emissão do Boleto
-$dadosboleto["data_processamento"] 	= date("d/m/Y"); // Data de processamento do boleto (opcional)
-$dadosboleto["valor_boleto"] 		= $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+$dadosboleto["nosso_numero"] 		= "";  							// Nosso numero sem o DV - REGRA: Máximo de 8 caracteres!
+$dadosboleto["numero_documento"] 	= "";							// Num do pedido ou do documento
+$dadosboleto["data_vencimento"] 	= $data_venc; 					// Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+$dadosboleto["data_documento"] 		= date("d/m/Y"); 				// Data de emissão do Boleto
+$dadosboleto["data_processamento"] 	= date("d/m/Y"); 				// Data de processamento do boleto (opcional)
+$dadosboleto["valor_boleto"] 		= $valor_boleto; 				// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
 
 // DADOS DO SEU CLIENTE
 // INSTRUÇÕES PARA O CAIXA
-$dadosboleto["instrucoes1"] = "- Após o vencimento cobrar 5% de multa e 0,033% de juros ao dia";
+$dadosboleto["instrucoes1"] = ""; 									//- Após o vencimento cobrar 5% de multa e 0,033% de juros ao dia
 $dadosboleto["instrucoes2"] = "";
 $dadosboleto["instrucoes3"] = "";
 $dadosboleto["instrucoes4"] = "";
@@ -34,17 +34,17 @@ $dadosboleto["especie_doc"]		= "";
 
 
 // DADOS DA SUA CONTA - BANESE
-$dadosboleto["agencia"] 	= "99"; // ABO 29 Num da agencia, sem digito
-//$dadosboleto["tipoconta"] 	= "03"; 	// 01 - Pessoa Fisica         03 - Pessoa Juridica
-$dadosboleto["conta"] 		= "999999999"; 	//ABO 031013307 Num da conta, sem digito 03102808-4
+$dadosboleto["agencia"] 	= "99"; 								// ABO 29 Num da agencia, sem digito
+$dadosboleto["tipoconta"] 	= "03"; 								// 01 - Pessoa Fisica 03 - Pessoa Juridica
+$dadosboleto["conta"] 		= "999999999"; 							// Num da conta, sem digito 99999999-9
 
 
 // SEUS DADOS
-$dadosboleto["identificacao"] 	= "Teste Gera Boloeto";
+$dadosboleto["identificacao"] 	= "";
 $dadosboleto["cpf_cnpj"] 		= "";
-$dadosboleto["endereco"] 		= "http://banese.com";
-$dadosboleto["cidade_uf"] 		= "Aracaju - SE";
-$dadosboleto["cedente"] 		= "Teste Gera Boloeto";
+$dadosboleto["endereco"] 		= "";
+$dadosboleto["cidade_uf"] 		= "";
+$dadosboleto["cedente"] 		= "";
 
 // NÃO ALTERAR!
 include("include/funcoes_banese.php"); 
